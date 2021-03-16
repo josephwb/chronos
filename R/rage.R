20db110abfa5c373f382b6d749e5b86c77d1834e
Joseph Brown <phylo.jwb@gmail.com>
2021-03-15 21:45:59 -0400

    fix example width

--- R/rage.R
+++ R/rage.R
@@ -28,20 +28,25 @@
 ##'     xlim=c(50,120), xlab="Clade Age",
 ##'     main="Strauss & Sadler Example\nn=1000, ages=c(50, 30, 25, 14, 3.5)");
 ##'   graphics::curve(dexp(x-50, rate=0.07), col='blue', lwd=2, add=TRUE);
-##'   graphics::legend("right", legend="dexp", col="blue", lty=1, lwd=2, box.lty=0);
+##'   graphics::legend("right", legend="dexp", col="blue", lty=1, lwd=2,
+##'     box.lty=0);
 ##'   
 ##'   # Example using the method of Solow (2003)
 ##'   hist(rage(n=10000, ages=c(50, 30, 25, 14, 3.5), method="Solow"),
 ##'     breaks=500, freq=FALSE, xlim=c(50,120), xlab="Clade Age",
 ##'     main="Solow Example\nn=1000, min.ages=c(50, 30, 25, 14, 3.5)");
-##'   graphics::curve(dexp(x-50, rate=0.04), col='blue', lwd=2, add=TRUE); # heavy on intermediate values
-##'   graphics::curve(dlnorm(x-50, meanlog=log(50-30), sdlog=pi/sqrt(3)), col='orange', lwd=2, add=TRUE);
-##'   graphics::legend("right", legend=c("dexp", "dlnorm"), col=c("blue", "orange"), lty=1, lwd=2, box.lty=0);
+##'     # heavy on intermediate values
+##'   graphics::curve(dexp(x-50, rate=0.04), col='blue', lwd=2, add=TRUE);
+##'   graphics::curve(dlnorm(x-50, meanlog=log(50-30), sdlog=pi/sqrt(3)),
+##'     col='orange', lwd=2, add=TRUE);
+##'   graphics::legend("right", legend=c("dexp", "dlnorm"), col=c("blue",
+##'     "orange"), lty=1, lwd=2, box.lty=0);
 ##'   
 ##'   # Strauss & Sadler (1989) method incorporating fossil age uncertainty
-##'   hist(rage(n=10000, ages=cbind(c(50, 30, 25, 14, 3.5), c(56, 35, 25, 14, 6))),
-##'     breaks=500, freq=FALSE, xlim=c(50,120), xlab="Age",
-##'     main="Solow Example (with fossil age uncertainty)n=1000,\nages=cbind(c(50, 30, 25, 14, 3.5), c(56, 35, 25, 14, 6))")
+##'   hist(rage(n=10000, ages=cbind(c(50, 30, 25, 14, 3.5),
+##'     c(56, 35, 25, 14, 6))), breaks=500, freq=FALSE, xlim=c(50,120), xlab="Age",
+##'     main="Solow Example (with fossil age uncertainty); n=1000,
+##'       ages=cbind(c(50, 30, 25, 14, 3.5), c(56, 35, 25, 14, 6))")
 ##'   graphics::curve(dlnorm(x-50, meanlog=2.6, sdlog=0.9), col='blue', lwd=2, add=TRUE);
 ##'   graphics::legend("right", legend="dlnorm", col="blue", lty=1, lwd=2, box.lty=0);
 ##'   }
